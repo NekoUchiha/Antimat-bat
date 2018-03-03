d8898cd62f88613577471c1a70b37ff8995e1e34
NekoUchiha <34886772+NekoUchiha@users.noreply.github.com>
2018-03-03 04:24:37 +0300

    Fix

--- matbot.js
+++ matbot.js
@@ -213,7 +213,7 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', (request, response) => {
     // ejs render automatically looks in the views folder
-    response.render('matbot');
+    response.render('Hello Worold');
 });
 
 app.listen(port, () => {
