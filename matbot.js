53a4b74e46d917f262250cab04cbb4745c150076
Neko <34886772+NekoUchiha@users.noreply.github.com>
2019-11-19 18:44:53 +0300

    test 3

--- matbot.js
+++ matbot.js
@@ -256,7 +256,8 @@ if (GuildName === "8MEC") return msg.channel.send(`Дорогие Девочки
 });
 
 
-robot.on('messageUpdate', async (msg, oldMessage, newMessage) => {
+robot.on('messageUpdate', async (oldMessage, newMessage) => {
+	if (newMessage === oldMessage) return undefined;
     if (newMessage.author.bot) return undefined;
 	
      if( swearWords.some(word => newMessage.content.toLowerCase().includes(word)) ) {
@@ -264,14 +265,14 @@ robot.on('messageUpdate', async (msg, oldMessage, newMessage) => {
     } else if( RrRu.some(word => newMessage.content.toLowerCase().includes(word)) ) {
 		if(NoBadWords.some(word => newMessage.content.toLowerCase().includes(word)) ) return;
 		            newMessage.delete();
-            newMessage.channel.send('**' + newMessage.author.username + '** - Ай яй яй Нельзя Говорить Такие Слова')
+            msg.channel.send('**' + newMessage.author.username + '** - Ай яй яй Нельзя Говорить Такие Слова')
             console.log(newMessage.author.username + ' '+ newMessage + ' ' + 'Удалено');
              robot.channels.find("id", process.env.LOG_CHANNALE).send(`**Лог**: Функция **delete Mat** - **${newMessage.author.username}** - **${newMessage}** Удалено`);
 			 robot.channels.find("id", process.env.DEL_MAT_CHANNALE).send(`**Удалён Мат** из Сервера **${newMessage.guild.name}[ID ${newMessage.guild.id}]** - в сообщении от - **${newMessage.author.username}** - **${newMessage}**`);
 	} else if ( sleepWords.some(word => newMessage.content.toLowerCase().includes(word)) ) {
-	newMessage.channel.send('Быстро Спать! :bed:');
+	msg.channel.send('Быстро Спать! :bed:');
 } else if ( innovaWords.some(word => newMessage.content.toLowerCase().includes(word)) ) {
-	newMessage.channel.send('Innova - Disconnected people :scream_cat: ');
+	msg.channel.send('Innova - Disconnected people :scream_cat: ');
 }
 });
 robot.login(process.env.TOKEN);
