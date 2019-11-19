6a6118df8787d2a45887bab0d93c4f2c48b820ac
Neko <34886772+NekoUchiha@users.noreply.github.com>
2019-11-19 18:52:17 +0300

    new version

--- matbot.js
+++ matbot.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 const comBD = require('./data/comBD.json', 'utf8');
 
-const BotVersion = "0.0.25";
+const BotVersion = "0.0.26";
 
 const swearWords = ["гей", "г е й", "геи", "гeи", "пидор", "гея", "пидоры", "пидр", "педик", "пидора", "проститука", "проституточка", "проституток", "проституки", "задрот", "пидарок", "пидop", "педор"];
 
@@ -261,18 +261,18 @@ robot.on('messageUpdate', async (oldMessage, newMessage) => {
     if (newMessage.author.bot) return undefined;
 	
      if( swearWords.some(word => newMessage.content.toLowerCase().includes(word)) ) {
-        msg.channel.send('Сама Такая')
+        newMessage.channel.send('Сама Такая')
     } else if( RrRu.some(word => newMessage.content.toLowerCase().includes(word)) ) {
 		if(NoBadWords.some(word => newMessage.content.toLowerCase().includes(word)) ) return;
 		            newMessage.delete();
-            msg.channel.send('**' + newMessage.author.username + '** - Ай яй яй Нельзя Говорить Такие Слова')
+            newMessage.channel.send('**' + newMessage.author.username + '** - Ай яй яй Нельзя Говорить Такие Слова')
             console.log(newMessage.author.username + ' '+ newMessage + ' ' + 'Удалено');
              robot.channels.find("id", process.env.LOG_CHANNALE).send(`**Лог**: Функция **delete Mat** - **${newMessage.author.username}** - **${newMessage}** Удалено`);
 			 robot.channels.find("id", process.env.DEL_MAT_CHANNALE).send(`**Удалён Мат** из Сервера **${newMessage.guild.name}[ID ${newMessage.guild.id}]** - в сообщении от - **${newMessage.author.username}** - **${newMessage}**`);
 	} else if ( sleepWords.some(word => newMessage.content.toLowerCase().includes(word)) ) {
-	msg.channel.send('Быстро Спать! :bed:');
+	newMessage.channel.send('Быстро Спать! :bed:');
 } else if ( innovaWords.some(word => newMessage.content.toLowerCase().includes(word)) ) {
-	msg.channel.send('Innova - Disconnected people :scream_cat: ');
+	newMessage.channel.send('Innova - Disconnected people :scream_cat: ');
 }
 });
 robot.login(process.env.TOKEN);
